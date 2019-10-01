d135b3e04159f1f8da554259e9297ec776507f64
Mark Lindsay <mark@octopart.com>
2019-10-01 14:04:07 +0000

    Number one commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Index = () => (
+  <>
+    <Head>
+      <title>
+        Frontend Exercise - Octopart
+      </title>
+    </Head>
+    <h1>It works!</h1>
+  </>
+)
+
+export default Index
